c82b96983cf4c3601e8ba829e240e5e828347e9b
Urbano Ortegaaa <136280600+UrbanoDino@users.noreply.github.com>
2024-11-02 12:03:46 -0600

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MongoDBAPI.Model
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string Category { get; set; }
+    }
+}
